fc4dc73d9fc293d4a05d3911f7277bf293d046ac
Carlos Asprilla <Carlos Asprilla@DESKTOP-H9J8BK7>
2017-10-21 13:49:53 -0500

    intente implementar la creacion del menu  y etc. con los platillos

--- src/objects/Platillo.java
+++ src/objects/Platillo.java
@@ -23,4 +23,83 @@ public class Platillo {
     
     private ImageIcon foto;
     
+    public Platillo(String name , String descr, int precio , ImageIcon fot){
+        nombre = name;
+        descripcion = descr;
+        this.precio = precio;
+        foto = fot;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the precio
+     */
+    public int getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    /**
+     * @return the estado
+     */
+    public boolean isEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    /**
+     * @return the foto
+     */
+    public ImageIcon getFoto() {
+        return foto;
+    }
+
+    /**
+     * @param foto the foto to set
+     */
+    public void setFoto(ImageIcon foto) {
+        this.foto = foto;
+    }
+    public String ToString(){
+     return nombre;   
+    }
 }
